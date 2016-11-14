7fcc8dd26bdb398bde4ec5f355d40a2b1848d8f2
Mustafa Kamal <mustafa.kamal@aalto.fi>
2016-11-14 23:36:40 +0200

    keyword_usage

---
+++ exercises/04_python/keyword_usage.py
@@ -0,0 +1,11 @@
+def keyword_usage( string, keywords ):
+	
+	result = ()
+	
+	for keyword in keywords:
+		if keyword in string:
+			result = result + (True,)
+		else:
+			result = result + (False,)
+	
+	print( result )
