d2973ae017149e557ae118522d64f4f94b0abbdd
Mustafa Kamal <mustafa.kamal@aalto.fi>
2016-11-15 00:45:55 +0200

    keyword_usage change to list comparison

--- exercises/04_python/keyword_usage.py
+++ exercises/04_python/keyword_usage.py
@@ -1,9 +1,10 @@
 def keyword_usage( string, keywords ):
 	
+	words = string.split()
 	result = ()
 	
 	for keyword in keywords:
-		if keyword in string:
+		if keyword in words:
 			result = result + (True,)
 		else:
 			result = result + (False,)
