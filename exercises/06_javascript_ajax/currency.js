454b32236a45358abd97207be034964a66bae8fe
Kamal <mustafakamal87@gmail.com>
2016-12-12 12:57:42 +0200

    change from using .ajax to .getJSON

--- exercises/06_javascript_ajax/currency.js
+++ exercises/06_javascript_ajax/currency.js
@@ -2,15 +2,24 @@ $(function(){
   $("#search").on("click", function(e){
     e.preventDefault();
 
-    $.ajax({
-      dataType: "json",
-      url: "http://api.fixer.io/"+$("#date").val(),
-      success: function(data){
-        $("#currencies").html("");
-        $.each(data.rates, function(idx, val){
-          $("#currencies").append("<tr><td>"+idx+"</td><td>"+val+"</td></tr>");
-        });
-      }
+    $.getJSON( "http://api.fixer.io/"+$("#date").val(), function( data ) {
+      $("#currencies").html("");
+      $.each(data.rates, function(idx, val){
+        $("#currencies").append("<tr><td>"+idx+"</td><td>"+val+"</td></tr>");
+      });
     });
+
+    // $.ajax({
+    //   dataType: "jsonp",
+    //   url: "http://api.fixer.io/"+$("#date").val(),
+    //   success: function(data){
+    //     $("#currencies").html("");
+    //     $.each(data.rates, function(idx, val){
+    //       $("#currencies").append("<tr><td>"+idx+"</td><td>"+val+"</td></tr>");
+    //     });
+    //   }
+    // });
+
+
   })
 });
