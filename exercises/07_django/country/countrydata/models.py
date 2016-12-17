1dc179f30fcffc62c37959113520cbd24dd5fdf7
Kamal <mustafakamal87@gmail.com>
2016-12-17 10:16:36 +0200

    django model for exercise 7.1

--- exercises/07_django/country/countrydata/models.py
+++ exercises/07_django/country/countrydata/models.py
@@ -3,9 +3,21 @@ from django.db import models
 
 class Continent(models.Model):
     """ Write your answer in 7.1 here. """
-    pass
+    name = models.CharField(max_length=20,unique=True)
+    code = models.CharField(max_length=2,unique=True)
+
+    class Meta:
+        ordering = ["name"]
 
 
 class Country(models.Model):
     """ Write your answer in 7.1 here. """
-    pass
+    name = models.CharField(max_length=20)
+    code = models.CharField(max_length=2,unique=True)
+    capital = models.CharField(max_length=100)
+    population = models.PositiveIntegerField(default=0)
+    area = models.PositiveIntegerField(default=0)
+    continent = models.ForeignKey(Continent, on_delete=models.CASCADE, related_name='countries')
+
+    class Meta:
+        ordering = ["name"]
