66025b013dac6d9a0b11da300ba2a91fd0141603
Kamal <mustafakamal87@gmail.com>
2016-12-17 13:41:24 +0200

    exercise 7.2

--- exercises/07_django/country/countrydata/views.py
+++ exercises/07_django/country/countrydata/views.py
@@ -1,14 +1,48 @@
-from django.http import Http404
+from django.http import HttpResponse, Http404
+from pprint import pprint
 import json
+from django.core import serializers
 
 from .models import Continent, Country
 
 
 def continent_json(request, continent_code):
-    """ Write your answer in 7.2 here. """
-    raise Http404("Not implemented")
+    try:
+        continent_data = Continent.objects.get(code=continent_code)
+        countries_of_continent = continent_data.countries.all()
+
+        output = {}
+        for c in countries_of_continent:
+            output[c.code] = c.name;
+
+        json_dump = json.dumps(output)
+
+        callback = request.GET.get('callback')
+        if callback is not None:
+            json_dump = callback + "("+json_dump+")"
+
+        return HttpResponse(json_dump, content_type="application/json")
+
+    except Continent.DoesNotExist:
+        raise Http404("Continent does not exist")
 
 
 def country_json(request, continent_code, country_code):
-    """ Write your answer in 7.2 here. """
-    raise Http404("Not implemented")
+    try:
+        country = Country.objects.get(code=country_code)
+
+        output = {
+            "area": country.area,
+            "population": country.population,
+            "capital": country.capital
+        }
+
+        json_dump = json.dumps(output)
+
+        callback = request.GET.get('callback')
+        if callback is not None:
+            json_dump = callback + "("+json_dump+")"
+
+        return HttpResponse(json_dump, content_type="application/json")
+    except Country.DoesNotExist:
+        raise Http404("Country does not exist")
