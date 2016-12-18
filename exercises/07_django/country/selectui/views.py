0eb8a9b95956f0a4f9967f5c08047a560f0aca94
Kamal <mustafakamal87@gmail.com>
2016-12-18 18:41:40 +0200

    exercise 7.3

--- exercises/07_django/country/selectui/views.py
+++ exercises/07_django/country/selectui/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 from countrydata.models import Continent
-
+from django.core import serializers
 
 def show_continent(request, continent_code=None):
     context = {
@@ -10,6 +10,11 @@ def show_continent(request, continent_code=None):
     if continent_code:
         continent = get_object_or_404(Continent, code=continent_code)
         context["continent"] = continent
+        countries = continent.countries.all()
+        context["countries"] = countries
 
     # Add your answer in 7.3 here
-    return render(request, "selectui/index.html", context)
+    if request.is_ajax():
+        return render(request, "selectui/countrytable.html", context)
+    else:
+        return render(request, "selectui/index.html", context)
